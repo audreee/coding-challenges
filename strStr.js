71aea11f2fb4013c281e075fc75c27c3cd98d678
Audree Steinberg <audreesteinberg@gmail.com>
2021-04-27 19:39:34 -0700

    Complete strStr

---
+++ strStr.js
@@ -0,0 +1,8 @@
+/* Implement strStr().
+
+Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+*/
+
+var strStr = function(haystack, needle) {
+  return haystack.indexOf(needle)
+};
