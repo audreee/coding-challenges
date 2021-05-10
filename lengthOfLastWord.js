e09a1701c0573f118c54abbebecd451360d199c1
Audree Steinberg <audreesteinberg@gmail.com>
2021-05-09 21:05:41 -0700

    Complete lengthOfLastWord

---
+++ lengthOfLastWord.js
@@ -0,0 +1,11 @@
+/* Given a string s consists of some words separated by spaces, return the length of the last word in the string. If the last word does not exist, return 0.
+
+A word is a maximal substring consisting of non-space characters only.
+
+*/
+
+var lengthOfLastWord = function(s) {
+  s = s.trim();
+  let array = s.split(' ');
+  return array[array.length - 1].length
+};
